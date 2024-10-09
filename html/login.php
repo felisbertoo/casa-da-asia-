f28539aa23ff61476f1876595b9dd794c011db41
felisbertoo <160753398+felisbertoo@users.noreply.github.com>
2024-10-09 11:42:54 +0000

    correção tela de perfil

--- html/login.php
+++ html/login.php
@@ -22,3 +22,18 @@
     <div> </div>
 </body>
 </html>
+
+<?php
+$conexao=mysqli_connect("localhost","root","","casadaasia");
+if(isset($_POST["login"])){      
+$email=$_POST["email"];   
+   $senha=$_POST["senha"];
+   $sql="SELECT * FROM `cliente` WHERE email='$email' and  senha='$senha'";
+    $resultado = mysqli_query($conexao,$sql);
+    while($linha=mysqli_fetch_array($resultado)){
+        if ($linha){ echo "email registrado";
+        echo "<br>";}
+                else {echo "Email não encontrado";}      
+    };
+}
+?>
